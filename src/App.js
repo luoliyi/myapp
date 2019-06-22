7602b4d89c6fbfe1475a47071679335d99bbe205
luoliyi <mopln510@qq.com>
2019-06-22 19:08:41 +0800

    test standard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.svg'
 import TodoParent from './views/TodoParent'
-import './App.css';
+import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <TodoParent/>
-        </div>
-    );
+function App () {
+  return (
+    <div className='App'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <TodoParent />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/views/TodoInput.js
+++ src/views/TodoInput.js
@@ -1,36 +1,36 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 export default class TodoInput extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            val: ''
-        }
-        this.getValue = this.getValue.bind(this)
-        this.sendToPar = this.sendToPar.bind(this)
+  constructor (props) {
+    super(props)
+    this.state = {
+      val: ''
     }
+    this.getValue = this.getValue.bind(this)
+    this.sendToPar = this.sendToPar.bind(this)
+  }
 
-    getValue(e) {
-        this.setState({
-            val: e.target.value
-        })
-    }
+  getValue (e) {
+    this.setState({
+      val: e.target.value
+    })
+  }
 
-    sendToPar() {
-        if (this.state.val != '') {
-            this.props.getChildVal(this.state.val)
-        }
+  sendToPar () {
+    if (this.state.val !== '') {
+      this.props.getChildVal(this.state.val)
     }
+  }
 
-    componentDidMount() {
-    }
+  componentDidMount () {
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>留下评论</h3>
-                <input type='text' value={this.state.val} onChange={this.getValue}/>
-                <button onClick={this.sendToPar}>提交</button>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <h3>留下评论</h3>
+        <input type='text' value={this.state.val} onChange={this.getValue} />
+        <button onClick={this.sendToPar}>提交</button>
+      </div>
+    )
+  }
 }
